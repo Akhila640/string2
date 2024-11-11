201f35bd426c7032dd3694b0d765fe9f599977de
Akhila640 <akhilasiddi00@gmail.com>
2024-11-11 12:52:10 +0530

    Add files via upload

---
+++ string2.js
@@ -0,0 +1,19 @@
+// 1.QUESTION
+let str = "Hello World!";
+console.log(str.replace("Hello World!","My Dear Friend"));
+
+// // 2.QUESTION
+let a="Hungry ";
+console.log(a.repeat(10));
+
+// 3.QUESTION
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+console.log(sentence.lastIndexOf("fox"));
+
+// 4.QUESTION
+let message = "Hello, world!";
+console.log(message.startsWith("hi"));
+
+// 5.QUESTION
+let fileName = "document.pdf";
+console.log(fileName.endsWith(".pdf"));
